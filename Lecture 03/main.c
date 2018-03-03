aeeb5e002c7a9d19c1db70cfc15a8f2a18c94857
javiergs <javiergs@jgs-macbook-pro.local>
2018-03-03 09:04:41 -0700

    Update Spring 2018

--- Lecture 03/main.c
+++ Lecture 03/main.c
@@ -1,6 +1,6 @@
 /*
-Lecture 03: Hello World with OpenGL
-http://javiergs.com/teaching/ser332
+ * Lecture 03: Hello World with OpenGL
+ * http://javiergs.com/teaching/ser332
 */
 
 #if defined(__APPLE__)
@@ -9,39 +9,43 @@ http://javiergs.com/teaching/ser332
 #include <GL/glut.h>
 #endif
 
-// myInit
+/**
+ *  myInit
+ */
 void myInit() {
   glClearColor(0.764f, 0.129f, 0.282f, 1.0); // color maroon
-  glColor3f(1.0f, 0.843f, 0.0f);  			// color gold
-  glMatrixMode(GL_PROJECTION);  				// projection
+  glColor3f(1.0f, 0.843f, 0.0f); // color gold
+  glMatrixMode(GL_PROJECTION); // projection
   glLoadIdentity();
-  gluOrtho2D(-1.0, 1.0, -1.0, 1.0); 			// units inside
+  gluOrtho2D(-1.0, 1.0, -1.0, 1.0); // units inside
 }
 
-// myDisplay
+/**
+ *  myDisplay
+ */
 void myDisplay() {
-  glClear(GL_COLOR_BUFFER_BIT); 				// clear the window
-  glBegin(GL_POLYGON);  						// fill connected polygon
-  	  glVertex2f(-0.7, 0.7);  				// vertices of the triangle 1
+  glClear(GL_COLOR_BUFFER_BIT); // clear the window
+  glBegin(GL_POLYGON); // fill connected polygon
+  	  glVertex2f(-0.7, 0.7); // vertices of the triangle 1
   	  glVertex2f(0.6, 0.7);
   	  glVertex2f(-0.7, -0.6);
   glEnd();
-  glBegin(GL_POLYGON);  						// fill connected polygon
-  	  glVertex2f(0.7, 0.6);  				// vertices of the triangle 2
+  glBegin(GL_POLYGON); // fill connected polygon
+  	  glVertex2f(0.7, 0.6); // vertices of the triangle 2
   	  glVertex2f(-0.6, -0.7);
   	  glVertex2f(0.7, -0.7);
   glEnd();
-  glFlush();  								//forces commands to execute
+  glFlush(); //forces commands to execute
 }
 
 // main
 void main(int argc, char** argv) {
-  glutInit(&argc, argv); 					// glut init
+  glutInit(&argc, argv); // glut init
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(500, 500); 				// actual window size
-  glutInitWindowPosition(0, 0);				// window location
+  glutInitWindowSize(500, 500); // actual window size
+  glutInitWindowPosition(0, 0); // window location
   glutCreateWindow("Lecture 03");
   myInit();
   glutDisplayFunc(myDisplay);
-  glutMainLoop(); 							//event loop
+  glutMainLoop(); //event loop
 }
